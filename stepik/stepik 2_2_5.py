5fbead9b99bd00f0ecdda889fdd7be09dfdeca2b
vittorio5 <vittorio.inbox@gmail.com>
2022-04-24 22:13:00 +0700

    Начато прохождение 2-го модуля

--- stepik/stepik 2_2_5.py
+++ stepik/stepik 2_2_5.py
@@ -1,12 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
 
 
 browser = webdriver.Chrome()
 browser.get('http://suninjuly.github.io/selects1.html')
-num1 = browser.find_element_by_id('num1').text
-num2 = browser.find_element_by_id('num2').text
+num1 = browser.find_element(By.ID, 'num1').text
+num2 = browser.find_element(By.ID, 'num2').text
 x = int(num1) + int(num2)
-select = Select(browser.find_element_by_id("dropdown"))
+select = Select(browser.find_element(By.ID, "dropdown"))
 select.select_by_value(str(x))
-browser.find_element_by_tag_name('button').click()
+browser.find_element(By.TAG_NAME, 'button').click()
