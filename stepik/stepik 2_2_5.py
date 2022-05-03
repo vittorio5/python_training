3ce17bcf184d734dfe40273f63fc6802d9045139
vittorio5 <vittorio.inbox@gmail.com>
2022-05-03 10:39:33 +0700

    Продолжено прохождение 2-го модуля

--- stepik/stepik 2_2_5.py
+++ stepik/stepik 2_2_5.py
@@ -1,13 +1,9 @@
 from selenium import webdriver
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.common.by import By
-
 
 browser = webdriver.Chrome()
-browser.get('http://suninjuly.github.io/selects1.html')
-num1 = browser.find_element(By.ID, 'num1').text
-num2 = browser.find_element(By.ID, 'num2').text
-x = int(num1) + int(num2)
-select = Select(browser.find_element(By.ID, "dropdown"))
-select.select_by_value(str(x))
-browser.find_element(By.TAG_NAME, 'button').click()
+link = "https://SunInJuly.github.io/execute_script.html"
+browser.get(link)
+button = browser.find_element_by_tag_name("button")
+browser.execute_script("return arguments[0].scrollIntoView(true);", button)
+# browser.execute_script("window.scrollBy(0, 100);")
+button.click()
